0802a484064afe252b6c796b3798884d0569994c
AlizadehAli <a.alizade@live.com>
2020-01-17 18:07:56 +0300

    added ParticleFilter::init

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -32,7 +32,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 200;  // TODO: Set the number of particles
+  num_particles = 100;  // TODO: Set the number of particles
 
   double std_x std_y std_theta
   std_x = std[0];
